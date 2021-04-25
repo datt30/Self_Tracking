a78522ab7f89661f4c611af380f43eb26d8adce6
datt30 <davidtamayo30@gmail.com>
2021-04-25 16:56:36 -0500

    report support

--- report.py
+++ report.py
@@ -3,12 +3,32 @@ import jinja2
 import webbrowser
 import statistics as st
 
+BLINKS_PER_MINUTE_RECOMMENDED = 20
+BLINK_POSITIVE_MESSAGE = "Optimal, above that recommended by the WHO"
+BLINK_WARNING_MESSAGE = "Below that recommended by the WHO"
+DANGER_TEXT_COLOR_CLASS = "text-danger"
+SUCCESS_TEXT_COLOR_CLASS = "text-success"
+
+
+def get_blink_average_results(blink_average):
+    percentage = ((blink_average/BLINKS_PER_MINUTE_RECOMMENDED)*100) - 100
+    message = BLINK_WARNING_MESSAGE if blink_average < BLINKS_PER_MINUTE_RECOMMENDED else BLINK_POSITIVE_MESSAGE
+    color = DANGER_TEXT_COLOR_CLASS if blink_average < BLINKS_PER_MINUTE_RECOMMENDED else SUCCESS_TEXT_COLOR_CLASS
+    return percentage, message, color
+
 
 def generate_report(blink_data, body_position_data):
-    if blink_data:
+    if blink_data and body_position_data:
+
+        blink_average = round(st.mean(blink_data))
+        blink_percentage, blink_message, blink_color = get_blink_average_results(blink_average)
+
         data = {
             'time': len(blink_data),
-            'average_blink': round(st.mean(blink_data)),
+            'blink_average': blink_average,
+            'blink_percentage': blink_percentage,
+            'blink_message': blink_message,
+            'blink_color': blink_color,
             'upper_blink_ratio': max(blink_data),
             'lower_blink_ratio': min(blink_data),
             'blink_labels': list(range(1, len(blink_data)+1)),
